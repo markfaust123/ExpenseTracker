f01a58ca656cf6b4f958a8ac8500662f008b99b6
Mark Faust <markfaust123@gmail.com>
2024-01-10 12:17:23 -0500

    added React Navigation packages and basic setup to app

---
+++ src/screens/AllExpenses.tsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+const AllExpenses = () => {
+  return <Text>ManageExpenses Screen</Text>;
+};
+
+export default AllExpenses;
---
+++ src/screens/ManageExpenses.tsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+const ManageExpenses = () => {
+  return <Text>ManageExpenses Screen</Text>;
+};
+
+export default ManageExpenses;
